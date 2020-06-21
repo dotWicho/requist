bfda5f2ac069ff9d6ea01cee8a2adef3963a5bee
Luis Martinez <dotwicho@gmail.com>
2020-06-21 11:25:06 -0300

    Changes for to be able to use outside the package

--- constants.go
+++ constants.go
@@ -7,9 +7,9 @@ import "time"
 const (
 	acceptHeader    string = "Accept"
 	contentType     string = "Content-Type"
-	textContentType string = "text/plain"
-	jsonContentType string = "application/json"
-	formContentType string = "application/x-www-form-urlencoded"
+	TextContentType string = "text/plain"
+	JsonContentType string = "application/json"
+	FormContentType string = "application/x-www-form-urlencoded"
 
 	// Timeout of http.Client default, 4 seconds
 	defaultTimeout = 4 * time.Second
